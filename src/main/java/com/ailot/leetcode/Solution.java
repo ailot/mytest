8aac9dccf7278c1172652c9170b4278594899eea
litao <lt48615@ly.com>
2018-11-22 22:50:27 +0800

    resolve

--- src/main/java/com/ailot/leetcode/Solution.java
+++ src/main/java/com/ailot/leetcode/Solution.java
@@ -1,41 +1,52 @@
 package com.ailot.leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
 
-
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(8);
-
-        ListNode l2 = new ListNode(0);
-        addTwoNumbers(l1, l2);
+        int len = lengthOfLongestSubstring("abbsdearu");
+        System.out.println(len);
     }
 
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                return new int[]{map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode p = l1, q = l2, current = head;
         int carry = 0;
-        while (p != null && q != null) {
-            int x = (p!=null) ? p.val : 0;
-            int y = (q!=null) ? q.val : 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
             int sum = carry + x + y;
-            carry = sum/10;
-            current.next = new ListNode(sum%10);
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
             current = current.next;
-            if (p != null){
+            if (p != null) {
                 p = p.next;
             }
-            if (q != null){
+            if (q != null) {
                 q = q.next;
             }
         }
-        if (carry > 0){
+        if (carry > 0) {
             current.next = new ListNode(carry);
         }
         return head.next;
     }
-
 
     public static class ListNode {
         int val;
@@ -46,4 +57,21 @@ public class Solution {
         }
     }
 
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
 }
